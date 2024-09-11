ca5298b88e15586af0254506644e29481155cc56
Emily Villanueva <emily.villanueva6781@alumnos.udg.mx>
2024-09-11 13:53:31 -0600

    else return

--- app.py
+++ app.py
@@ -59,8 +59,10 @@ def agregarDatos():
         connection.commit()
         connection.close()
 
-        return redirect(url_for('index'))
-    return render_template('add_data.html')
+        return redirect(url_for('/'))
+    else:
+        return render_template('/add.html')
+    return render_template('/add.html')
 
 if __name__ = '__main__':
     app.run(debug=True)
